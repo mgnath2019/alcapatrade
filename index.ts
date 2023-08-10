393ec24ccd0bd3d1e89c7cb6137845ceaea1412a
Gopinath <gopinath.manchineella@gmail.com>
2023-08-09 20:28:38 -0400

    Updated the .gitignore file

---
+++ index.ts
@@ -0,0 +1,22 @@
+// Import the express in typescript file
+import express from 'express';
+ 
+// Initialize the express engine
+const app: express.Application = express();
+ 
+// Take a port 3000 for running server.
+const port: number = 3000;
+ 
+// Handling '/' Request
+app.get('/', (_req, _res) => {
+    _res.send("Alcapa Trade API v0.1");
+});
+app.post('/account', (_req, _res) => {
+    _res.send("TypeScript With Express");
+});
+ 
+// Server setup
+app.listen(port, () => {
+    console.log(`TypeScript with Express
+         http://localhost:${port}/`);
+});
