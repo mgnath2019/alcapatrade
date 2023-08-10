f4c8b6c2862f3f3bc0eabc2464b8cfc08a64cbca
Gopinath <gopinath.manchineella@gmail.com>
2023-08-09 20:59:32 -0400

    paper trade

--- index.ts
+++ index.ts
@@ -6,13 +6,21 @@ const app: express.Application = express();
  
 // Take a port 3000 for running server.
 const port: number = 3000;
- 
+
+const Alpaca = require('@alpacahq/alpaca-trade-api')
+const alpaca = new Alpaca({
+    keyId: 'PKV7FQB9BKEDBGZQUCP7',
+    secretKey: 'W1s6ja9iHbZUTLCEgdwL9btcbfFaR7Oe3hBvhbgZ',
+    paper: true,
+  });
+
 // Handling '/' Request
 app.get('/', (_req, _res) => {
     _res.send("Alcapa Trade API v0.1");
 });
-app.post('/account', (_req, _res) => {
-    _res.send("TypeScript With Express");
+app.post('/account', async (_req, _res) => {
+    let acco =  await alpaca.getAccount();
+    return _res.send(acco); 
 });
  
 // Server setup
